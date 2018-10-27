d8ab48963c6c2fcb0ae469a27e05e408f6c1dbbd
kwasett <kwasett@gmail.com>
2018-10-27 04:12:01 +0100

    Service static files

--- web.js
+++ web.js
@@ -1,6 +1,16 @@
 var port = Number(process.env.PORT || 5000);
-var httpServer = require('http');
-var server = httpServer.createServer();
+
+var http = require('http');
+
+var finalhandler = require('finalhandler');
+var serveStatic = require('serve-static');
+
+var serve = serveStatic("./");
+
+var server = http.createServer(function(req, res) {
+  var done = finalhandler(req, res);
+  serve(req, res, done);
+});
 server.listen(port, '0.0.0.0', function() {
     console.log('Listening on port '+port+'. Hit CTRL-C to stop the server.');
 });
